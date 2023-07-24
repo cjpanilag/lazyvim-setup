457e71e5075d3771ffa3dea1293f7b623aea332f
Carl Jeffrie Panilag <carljeffrie.panilag@fligno.com>
2023-07-24 22:50:25 +0800

    :fire: added new colorscheme configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,4 +45,19 @@ require("lazy").setup({
     },
 })
 
-vim.cmd("colorscheme sherbet")
+require("lualine").setup({
+    options = {
+        theme = "auto",
+    },
+})
+
+require("bluloco").setup({
+    style = "auto", -- "auto" | "dark" | "light"
+    transparent = false,
+    italics = false,
+    terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
+    guicursor = true,
+})
+
+vim.opt.termguicolors = true
+vim.cmd("colorscheme bluloco-dark")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,14 @@
 return {
     {
+        "uloco/bluloco.nvim",
+        lazy = false,
+        priority = 1000,
+        dependencies = { "rktjmp/lush.nvim" },
+        config = function()
+            -- your optional config goes here, see below.
+        end,
+    },
+    {
         "olimorris/onedarkpro.nvim",
         priority = 1000, -- Ensure it loads first
     },
